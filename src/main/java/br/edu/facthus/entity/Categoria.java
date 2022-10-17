407471d75174a12b2bfc5dc2352b6ecbcbcb9374
Fernando Mattioli <fernando.mattioli@facthus.edu.br>
2022-10-17 20:09:33 -0300

    Fix #8: inserção de categorias

--- src/main/java/br/edu/facthus/entity/Categoria.java
+++ src/main/java/br/edu/facthus/entity/Categoria.java
@@ -6,8 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQuery(name = "Categoria.findAll",
+    query = "SELECT c "
+        + "FROM Categoria c")
 public class Categoria implements Serializable {
 
     @Id
